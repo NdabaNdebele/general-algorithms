47bdc6eff64b23fe27344ddc2b46d8befeda9037
Ndaba Ndebele <ndabandebele@gmail.com>
2016-12-28 23:04:59 +0200

    Update steps.py

--- steps.py
+++ steps.py
@@ -2,24 +2,24 @@
 #Solving step algorithm problem 
 
 #recursively solves step algorithm
-def printSteps(ans,i,n):
+def printSteps(res,n,i):
     if(n < 0):
         return
     if(n == 0):
         ansStr = ""
         for h in range(0,i):
-            ansStr += ans[h]
+            ansStr += str(res[h])
         print ansStr
         return
     
-    ans[i] = '1'
-    printSteps(ans,n -1,i++)
-    ans[i] = '2'
-    printSteps(ans,n - 2,i++)
+    res[i] = '1'
+    printSteps(res,n -1,i+1)
+    res[i] = '2'
+    printSteps(res,n - 2,i+1)
 
 n = 4
 res = [0 for x in range(n)]
-pathPrintUtil(res,0,n)
+printSteps(res,n,0)
 
 
     
